7bccd3a796abe1cf414d79529a1a7a0288026444
waranya-sun <waranya.sun@gmail.com>
2022-08-29 00:30:04 +1000

    change header to be anchor tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header className='w-full text-center bg-black text-white py-4 relative'>
       <div>
-        <Link className='no-underline text-white text-xl font-bold' to='/'>
+        <a className='no-underline text-white text-xl font-bold' href='/'>
           <h2>Feedback Form</h2>
-        </Link>
+        </a>
         <Link
           to='/about'
           className='absolute right-5 top-4 bg-amber-400 py-1 px-2 rounded text-white font-bold no-underline'
